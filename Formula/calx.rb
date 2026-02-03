a1b24e1382a45e0845249e977a55662ee5eb5c09
ocolunga <oscar@ancile.dev>
2026-02-02 21:27:36 -0600

    Initial calx formula (placeholder)

---
+++ Formula/calx.rb
@@ -0,0 +1,21 @@
+class Calx < Formula
+  include Language::Python::Virtualenv
+
+  desc "CLI tool for displaying calendar information with terminal graphics"
+  homepage "https://github.com/ocolunga/calendar-cli"
+  url "https://files.pythonhosted.org/packages/placeholder/calx-0.1.0.tar.gz"
+  sha256 "placeholder"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  # Resource blocks auto-populated by brew update-python-resources on first release
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"calx", "--help"
+  end
+end
