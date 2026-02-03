04c2fea72e3192bd959b3369686855ff59dd6f16
ocolunga <oscar@ancile.dev>
2026-02-02 21:44:06 -0600

    Rename formula to calx-cli to match PyPI package name

--- Formula/calx-cli.rb
+++ Formula/calx-cli.rb
@@ -1,8 +1,8 @@
-class Calx < Formula
+class CalxCli < Formula
   include Language::Python::Virtualenv
 
   desc "CLI tool for displaying calendar information with terminal graphics"
-  homepage "https://github.com/ocolunga/calendar-cli"
+  homepage "https://github.com/ocolunga/calx"
   url "https://files.pythonhosted.org/packages/placeholder/calx_cli-0.1.0.tar.gz"
   sha256 "placeholder"
   license "MIT"
