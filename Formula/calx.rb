59efc1c33bb1302559a35fb136be7a5b6756e30a
ocolunga <oscar@ancile.dev>
2026-02-02 21:41:58 -0600

    Update formula for calx-cli PyPI package name

--- Formula/calx.rb
+++ Formula/calx.rb
@@ -3,7 +3,7 @@ class Calx < Formula
 
   desc "CLI tool for displaying calendar information with terminal graphics"
   homepage "https://github.com/ocolunga/calendar-cli"
-  url "https://files.pythonhosted.org/packages/placeholder/calx-0.1.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/placeholder/calx_cli-0.1.0.tar.gz"
   sha256 "placeholder"
   license "MIT"
 
